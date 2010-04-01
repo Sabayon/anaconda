b57fc1ac8f05e367013ccc2f49041c1304432170
Fabio Erculiani <lxnay@sabayon.org>
2010-04-01 21:13:26 +0200

    [rpmUtils] remove writeRpmPlatform()

--- yuminstall.py
+++ yuminstall.py
@@ -1141,7 +1141,6 @@ reposdir=/etc/anaconda.repos.d,/tmp/updates/anaconda.repos.d,/tmp/product/anacon
            # FIXME: make sure that the rpmdb doesn't have stale locks :/
            iutil.resetRpmDb(anaconda.rootPath)
 
-        iutil.writeRpmPlatform()
         anaconda.backend.freetmp(anaconda)
         self.ayum = AnacondaYum(anaconda)
         self.ayum.setup()
@@ -1537,9 +1536,6 @@ reposdir=/etc/anaconda.repos.d,/tmp/updates/anaconda.repos.d,/tmp/product/anacon
 #            log.error("Error making directory %s: %s" % (i, msg))
 
         self.initLog(anaconda.rootPath)
-
-        # setup /etc/rpm/ for the post-install environment
-        iutil.writeRpmPlatform(anaconda.rootPath)
 
         try:
             # FIXME: making the /var/lib/rpm symlink here is a hack to
