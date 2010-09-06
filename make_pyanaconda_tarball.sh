6a99e59bd6ea330e191ead8827af0ea5d4514b68
Fabio Erculiani <lxnay@sabayon.org>
2010-09-06 09:12:41 +0200

    [build] another set of improvements to make_pyanaconda_tarball.sh

--- make_pyanaconda_tarball.sh
+++ make_pyanaconda_tarball.sh
@@ -2,13 +2,14 @@
 SABAYON_VER=${SABAYON_VER:-5.3}
 rm -rf tmp
 mkdir tmp
-sh autogen.sh
-./configure --prefix=/usr --disable-selinux --enable-selinux=no
-make
-make DESTDIR="${PWD}"/tmp install
-find "${PWD}"/tmp -name "*.py[co]" | xargs rm
-find "${PWD}"/tmp -name "*.*a" | xargs rm
-find "${PWD}"/tmp -name "*.so" | xargs rm
+sh autogen.sh && \
+	./configure --prefix=/usr --disable-selinux --enable-selinux=no && \
+	make && make DESTDIR="${PWD}"/tmp install && \
+	( find "${PWD}"/tmp -name "*.py[co]" | xargs rm ) && \
+	( find "${PWD}"/tmp -name "*.*a" | xargs rm ) && \
+	( find "${PWD}"/tmp -name "*.so" | xargs rm )
+[[ "${?}" != "0" ]] && exit 1
+
 pyanaconda_dir=$(find tmp -name "pyanaconda" -type d)
 [[ -d "${pyanaconda_dir}" ]] || { echo "ouch" && exit 1 }
 cd $(dirname "${pyanaconda_dir}")
