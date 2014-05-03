6a916d00f8f17b6dfa873b911d02e250fdd4e93a
Fabio Erculiani <lxnay@sabayon.org>
2014-05-03 18:55:36 +0100

    Move the first new installclass in place

---
+++ pyanaconda/installclasses/gnome.py
@@ -0,0 +1,53 @@
+#
+# gnome.py
+#
+# Copyright (C) 2014 Fabio Erculiani
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from pyanaconda.installclass import BaseInstallClass
+from pyanaconda import nm
+from pyanaconda.i18n import N_
+
+from sabayon import Entropy
+from sabayon.livecd import LiveCDCopyBackend
+
+class InstallClass(BaseInstallClass):
+
+    id = "sabayon_gnome"
+    name = N_("Sabayon GNOME Desktop")
+    sortPriority = 10000
+
+    _l10n_domain = "anaconda"
+
+    efi_dir = "sabayon"
+
+    dmrc = "gnome"
+    if Entropy().is_sabayon_steambox():
+        dmrc = "steambox"
+
+    # check if GNOME is available on the system
+    if not Entropy().is_installed("gnome-base/gnome-session"):
+        hidden = 1
+
+    def configure(self, anaconda):
+        BaseInstallClass.configure(self, anaconda)
+        BaseInstallClass.setDefaultPartitioning(self, anaconda.storage)
+
+    def getBackend(self):
+        return LiveCDCopyBackend
+
+    def __init__(self):
+        BaseInstallClass.__init__(self)
