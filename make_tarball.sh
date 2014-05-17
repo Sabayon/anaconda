64a4affd7ee2eb094b593df63edf01f5456b2024
Fabio Erculiani <lxnay@sabayon.org>
2014-05-17 09:07:51 +0100

    update tarball generation script

--- make_tarball.sh
+++ make_tarball.sh
@@ -10,5 +10,8 @@ fi
 git checkout -d archive-branch &> /dev/null
 git checkout -b archive-branch "anaconda-${ver}" || exit 1
 ./autogen.sh || exit 1
-make -j4 || exit 1
+./configure --prefix=/usr --mandir=/usr/share/man \
+	--infodir=/usr/share/info --datadir=/usr/share --sysconfdir=/etc \
+	--localstatedir=/var/lib --sbindir=/sbin --datarootdir=/usr/share \
+	--disable-static --enable-introspection --enable-gtk-doc || exit 1
 make dist || exit 1
