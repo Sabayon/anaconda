e0aaf007caa648531b526c891e40ccbfe88920ac
Fabio Erculiani <lxnay@sabayon.org>
2016-07-17 06:15:30 +0200

    Update update_po.sh, add make po-pull

--- update_po.sh
+++ update_po.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sh autogen.sh && ./configure --prefix=/usr --disable-selinux --enable-selinux=no && make -C po update-po && make distclean
+sh autogen.sh && ./configure --prefix=/usr --disable-selinux --enable-selinux=no && make po-pull && make -C po update-po && make distclean
