57fe0c55adae9f754f299c277761b11e17b8c649
mudler <mudler@sabayon.org>
2016-09-13 10:15:41 +0200

    bootloader.py: keep language and keyboard mapping settings

--- pyanaconda/bootloader.py
+++ pyanaconda/bootloader.py
@@ -256,7 +256,7 @@ class BootLoader(object):
                             "noht", "acpi", "video", "pci", "nodmraid",
                             "nompath", "nomodeset", "noiswmd", "fips",
                             "selinux", "biosdevname", "ipv6.disable",
-                            "net.ifnames"]
+                            "net.ifnames", "vconsole.keymap", "locale"]
     preserve_args = []
 
     _trusted_boot = False
