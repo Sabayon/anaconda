e95e6d8d0c903b16b40af7babf458176c35c7eec
Sławomir Nizio <slawomir.nizio@sabayon.org>
2016-09-22 21:41:42 +0200

    fix is_virtualbox() usage

--- pyanaconda/sabayon/utils.py
+++ pyanaconda/sabayon/utils.py
@@ -670,7 +670,7 @@ blacklist nouveau
         try:
             repo = self._backend.entropy.installed_repository()
 
-            if not self.is_virtualbox:
+            if not self.is_virtualbox():
                 self.remove_package("virtualbox-guest-additions")
 
             for package in packages:
