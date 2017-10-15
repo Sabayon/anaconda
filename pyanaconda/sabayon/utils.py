adfba1ba768693e4742a0aecf0cb1f4e6cdce400
Joost Ruis <joost.ruis@sabayon.org>
2017-10-15 22:11:42 +0200

    Remove app-admin/setools after install

--- pyanaconda/sabayon/utils.py
+++ pyanaconda/sabayon/utils.py
@@ -639,6 +639,7 @@ blacklist nouveau
             "app-admin/calamares-sabayon-branding",
             "app-admin/calamares-sabayon-base-modules",
             "app-admin/calamares",
+            "app-admin/setools",
             "app-i18n/langtable",
             "dev-libs/libreport",
             "dev-libs/satyr",
